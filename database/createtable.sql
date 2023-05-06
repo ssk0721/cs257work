e31f07b89dbc41a87e8f4e894c0f1e449708197d
ssk0721 <ssk0721>
2023-05-05 21:58:38 -0500

    added second table to database

--- database/createtable.sql
+++ database/createtable.sql
@@ -10,3 +10,18 @@ CREATE TABLE salaries (
     teamyearID TEXT NOT NULL
 );
 
+
+DROP TABLE IF EXISTS salarieswide;
+CREATE TABLE salarieswide (
+    team TEXT NOT NULL,
+    winpercentage DECIMAL(4,3) NOT NULL,
+    catchers TEXT NOT NULL,
+    infielders TEXT NOT NULL,
+    outfielders TEXT NOT NULL,
+    DH TEXT NOT NULL,
+    pitchers TEXT NOT NULL,
+    TotalPayroll INT NOT NULL,
+    teamyear SMALLINT NOT NULL,
+    roundmade SMALLINT NOT NULL
+)
+
